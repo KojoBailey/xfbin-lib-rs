7b098b61e4d8ffc4df7169a47187f593c11f3594
Kojo Bailey <kojomolojo@gmail.com>
2026-04-06 19:35:01 +0100

    Use enum named fields

--- src/error.rs
+++ src/error.rs
@@ -1,25 +1,17 @@
-pub struct MismatchedFileSignatureData {
-    given_file_signature: u32,
-}
-
-pub struct MismatchedVersionData {
-    given_version_data: u32,
-}
-
 pub enum XfbinError {
     NullFile,
     CutShort,
-    MismatchedFileSignature(MismatchedFileSignatureData),
-    MismatchedVersion(MismatchedVersionData),
+    MismatchedFileSignature { given_file_signature: u32 },
+    MismatchedVersion { given_version: u32 },
 }
 
 impl XfbinError {
     pub fn to_int(&self) -> u32 {
         match self {
-            XfbinError::NullFile                   => 000,
-            XfbinError::CutShort                   => 001,
-            XfbinError::MismatchedFileSignature(_) => 100,
-            XfbinError::MismatchedVersion(_)       => 101,
+            XfbinError::NullFile                    => 000,
+            XfbinError::CutShort                    => 001,
+            XfbinError::MismatchedFileSignature{..} => 100,
+            XfbinError::MismatchedVersion{..}       => 101,
         }
     }
 }
